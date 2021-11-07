d013404d7a871324f850f8252b657943aae56dc8
Gordon Zhou <68175104+complexape@users.noreply.github.com>
2021-11-06 21:22:11 -0700

    added ohmy

--- cogs/fun.py
+++ cogs/fun.py
@@ -18,6 +18,29 @@ class Fun(commands.Cog, name="fun"):
         self.bot = bot
     
     @commands.command()
+    async def ohmy(self, ctx, user: discord.Member = None):
+        if user == None:
+            user = ctx.author
+
+        asset = user.avatar_url_as(size=128)
+        data = BytesIO(await asset.read())
+        final_path = f'temp/ohmygif{user.id}.gif'
+        animated_gif = Image.open("media/ohmygoodness.gif")
+        img = Image.open(data).convert("RGBA").resize((220, 220), Image.ANTIALIAS)
+
+        frames = []
+        for frame in ImageSequence.Iterator(animated_gif):
+            frame = frame.copy()
+            final_img = Image.new('RGBA', animated_gif.size, (0, 0, 0, 0))
+            final_img.paste(frame, (0,0))
+            final_img.paste(img, (270,195), mask=img)
+            frames.append(final_img)
+        frames[0].save(final_path, save_all=True, append_images=frames[1:]) 
+
+        await ctx.send(file=discord.File(final_path))
+        os.remove(final_path)
+        
+    @commands.command()
     async def highfive(self, ctx, target: discord.Member = None):
         response = requests.get("https://api.waifu.pics/sfw/highfive")
         embed = discord.Embed(title=f"- {ctx.author.name.lower()} high fives {target.display_name.lower()}", color=0x36b8c9)
@@ -54,10 +77,6 @@ class Fun(commands.Cog, name="fun"):
         embed = discord.Embed(title=f"- {ctx.author.name.lower()} waves at {target.display_name.lower()}", color=0x38c936)
         embed.set_image(url=response.json()["url"])
         await ctx.send(embed=embed)
-
-        
-
-        
 
 def setup(bot):
     bot.add_cog(Fun(bot))
