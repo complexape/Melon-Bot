4d20c72a514837f80bed666280bb23c07500c108
Gordon Zhou <gordonzhou223@gmail.com>
2022-01-13 21:20:53 -0800

    class refactoring

--- helpers/db_models.py
+++ helpers/db_models.py
@@ -1,14 +1,6 @@
-from pymongo.collection import Collection
+from datetime import datetime
 
-from constants import DB, ZERODATE
-
-class DocNotFoundError(Exception):
-    def __init__(self):
-        super().__init__(f"document does not exist")
-
-class FieldNotFoundError(Exception):
-    def __init__(self):
-        super().__init__(f"'field does not exist")
+from constants import DB, ZEROSTR
 
 
 class DBGuild:
@@ -30,91 +22,81 @@ class DBGuild:
         members = self.collection.find(projection)
 
         # creates a DBMember instance for each member doc
-        return [DBMember(m["_id"], m["name"], self.collection) for m in members]
+        return [DBMember(m, self) for m in members]
 
     def get_sorted_values(self, field_key, is_reverse=True):
-        # gets all members, along with their target field
-        members = list(self.collection.find({}, {"_id": 1, "name": 1, field_key:1}))
-
-        # returns a sorted list of user dictionaries
+        # gets only the members that don't have the default empty value for the target field
+        members = list(self.collection.find({field_key: {"$ne": DBMember.BASE_FIELDS[field_key]}}))
         return sorted(members, key=lambda doc: doc[field_key], reverse=is_reverse)
 
     def delete_member(self, name):
-        if self.collection.count_documents({ "name": name }, limit = 1) == 0:
-            raise DocNotFoundError
-
         self.collection.delete_one({"name": name})
 
 class DBMember:
-    def __init__(self, id, name, guild_collection: Collection):
-        self.id = id
-        self.name = name
-        self.guild_collection = guild_collection
-        self.is_new = False
-        self.fields = {
-                "_id": self.id, # mandatory field
-                "name": self.name,
-                "totalvctime": ZERODATE,
-                "longestvctime": ZERODATE,
-                "lastjoined": "",
-                "firstjoined": "",
-                "birthday": ""
-            }
-        self.check_member()
+    BASE_FIELDS = {
+        "_id": "",
+        "name": "",
+        "totalvctime": ZEROSTR,
+        "longestvctime": ZEROSTR,
+        "lastjoined": "",
+        "firstjoined": "",
+        "birthday": "",
+        "totaldrip": 0,
+        "dripreset": ""
+    }
+
+    def __init__(self, document: dict, guild: DBGuild):
+        self.doc = document
+        self.id = document["_id"]
+        self.name = document["name"]
+        self.guild_collection = guild.collection
     
     @classmethod
-    def from_member(cls, member):
+    def from_new(cls, member):
         guild = DBGuild(member.guild.id)
-        db_member = cls(member.id, member.name, guild.collection)
-        return db_member
-    
-    def check_member(self):
-        # creates a new member document for the user's guild if doesn't already exist 
-        if self.guild_collection.count_documents({ '_id': self.id }, limit = 1) == 0:
-            self.guild_collection.insert_one(self.fields)
-            self.is_new = True
-            self.doc =  self.guild_collection.find_one({"_id": self.id})
-        else:
-            self.doc = self.guild_collection.find_one({"_id": self.id})
 
-            # keeps keys up-to-date in case there are any changes
-            self.update_keys()
+        if guild.collection.count_documents({ '_id': member.id }, limit = 1) == 0:
+            document = DBMember.BASE_FIELDS.update({
+                "_id": str(member.id),
+                "name": member.name
+            })
+            guild.collection.insert_one(document)
 
-            # helps keep names in the database up-to-date
-            self.check_name()
+        else: # updates database document if member already exists
+            document = guild.collection.find_one({"_id": member.id})
 
-    def check_name(self):
-        if self.doc["name"] != self.name:
-            self.update_field("name", self.name)
+            if document["name"] != member.name:
+                guild.collection.update_one({ "_id": member.id}, { "$set": { "name": member.name }})
+                document["name"] = member.name
+         
+            if document.keys() != DBMember.BASE_FIELDS.keys():
+                for key, value in DBMember.BASE_FIELDS.items():
+                    if key not in document.keys():
+                        guild.collection.update_one({ "_id": member.id}, { "$set": { key: value }})
+                        document[key] = value
 
-    def add_field(self, key, new_value=""):
-        self.guild_collection.update_one({ "_id": self.id}, { "$set": { key: new_value }})
+        return cls(document, guild)
+
+    @staticmethod
+    def value_as_datetime(str):
+        try:
+            return datetime.strptime(str , "%Y-%m-%d %H:%M:%S.%f")
+        except ValueError:
+            return datetime.strptime(str , "%m/%d/%Y") 
 
     def update_field(self, key, new_value=""):
-        if key not in self.doc.keys():
-            raise FieldNotFoundError(key, self.name)
-        
+        if isinstance(new_value, datetime):
+            new_value = new_value.strftime("%Y-%m-%d %H:%M:%S.%f")
+
         self.guild_collection.update_one({ "_id": self.id}, { "$set": { key: new_value }})
     
     def delete_field(self, key):
-        if key not in self.doc.keys():
-            raise FieldNotFoundError
-
         self.guild_collection.update_one({ }, {"$unset": { key: ""}})
     
-    def get_value(self, key):
-        if key not in self.doc.keys():
-            raise FieldNotFoundError
-        
-        return self.guild_collection.find_one({"_id": self.id})[key]
-
-    def update_keys(self):
-        keys = self.doc.keys()
-        new_keys = self.fields.keys()
-
-        if keys != new_keys:
-            # adds missing keys in member's document
-            [self.add_field(f, self.fields[f]) for f in new_keys if f not in keys]
-
-            # removes extra keys in member's document
-            [self.delete_field(f) for f in keys if f not in new_keys]
+    def get(self, key, as_dt=False):
+        value = self.doc[key]
+        return DBMember.value_as_datetime(value) if as_dt else value
+    
+    def is_new(self):
+        base_fields = DBMember.BASE_FIELDS
+        return self.doc == base_fields.update({"_id": self.id, "name": self.name})
